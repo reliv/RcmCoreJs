33592b3fac6bee718435d73e5a33ace0d8a832d8
James Jervis <jjervis@relivinc.com>
2016-08-03 10:24:00 -0500

    Update config

---
+++ config/asset_manager.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * asset_manager.php
+ */
+$bowerComponentsDir = __DIR__ . '/../../../../public/bower_components/';
+return [
+    'resolver_configs' => [
+        'aliases' => [
+            // Legacy support because public files used to be in RCM "core/public"
+            'modules/rcm/' => __DIR__ . '/../public/',
+            'modules/rcm-core-js/' => __DIR__ . '/../public/',
+        ],
+        'collections' => [
+            /**
+             * Core JS and css
+             * (core features)
+             */
+            'modules/rcm/rcm.js' => [],
+            'modules/rcm/rcm.css' => [
+                // Placeholder css
+                '/modules/rcm-core-js/core/rcm.css'
+            ],
+            /**
+             * Extended JS and css
+             * (features for modules and lower level services)
+             */
+            'modules/rcm/modules.js' => [],
+            'modules/rcm/modules.css' => [],
+        ],
+    ],
+];
