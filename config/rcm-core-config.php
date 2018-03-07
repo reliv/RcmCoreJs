2f7ded4e3b8c4727640f94937b537bc415ba705d
rodmcnew <rodmcnew@gmail.com>
2018-03-07 13:32:48 -0600

    combine js builds

--- config/rcm-core-config.php
+++ config/rcm-core-config.php
@@ -12,11 +12,11 @@ return [
          */
         'scripts' => [
             'libraries' => [
-                '/bower_components/jquery/dist/jquery.min.js' => [],
+//                '/bower_components/jquery/dist/jquery.min.js' => [], (moved to app webpack build)
                 '/bower_components/jquery-migrate/jquery-migrate.min.js' => [],
                 // @todo Move this config to the modules that use it
                 '/bower_components/jquery-ui/jquery-ui.min.js' => [],
-                '/bower_components/angular/angular.min.js' => [],
+//                '/node_modules/angular/angular.min.js' => [], (moved to app webpack build)
                 '/bower_components/angular-route/angular-route.min.js' => [],
                 '/bower_components/bootbox/bootbox.js' => [],
                 '/bower_components/bootstrap/dist/js/bootstrap.min.js' => [],
